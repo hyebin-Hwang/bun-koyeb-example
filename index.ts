32a6b25d41007af52fdaf4b25cd066098ae2aaec
hyebinHwang <sksp4334@naver.com>
2024-01-11 01:00:13 +0900

    Fix: 예제 코드 수정

--- index.ts
+++ index.ts
@@ -10,33 +10,40 @@ import type { ServerWebSocket } from "bun";
 
 const clients = new Set<ServerWebSocket>();
 
+// const server = Bun.serve({
+//   port: 3001,
+//   fetch(req, server) {
+//     if (server.upgrade(req)) {
+//       return; // do not return a Response
+//     }
+//     return new Response("Upgrade failed :(", { status: 500 });
+//   },
+//   websocket: {
+//     message(ws, message) {
+//       clients.forEach((client) => {
+//         if (client.readyState === WebSocket.OPEN) {
+//           client.send(message);
+//         }
+//       });
+//     }, // a message is received
+//     open(ws: ServerWebSocket) {
+//       clients.add(ws);
+//       console.log("OPEN WEBSOCKET?");
+//     }, // a socket is opened
+//     close(ws, code, message) {
+//       console.log("CLOSE WEBSOCKET");
+//     }, // a socket is closed
+//     drain(ws) {
+//       console.log("READY SOCKET");
+//     }, // the socket is ready to receive more data
+//   }, // handlers
+// });
+
 const server = Bun.serve({
   port: 3001,
   fetch(req, server) {
-    if (server.upgrade(req)) {
-      return; // do not return a Response
-    }
-    return new Response("Upgrade failed :(", { status: 500 });
+    return new Response("Success!");
   },
-  websocket: {
-    message(ws, message) {
-      clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(message);
-        }
-      });
-    }, // a message is received
-    open(ws: ServerWebSocket) {
-      clients.add(ws);
-      console.log("OPEN WEBSOCKET?");
-    }, // a socket is opened
-    close(ws, code, message) {
-      console.log("CLOSE WEBSOCKET");
-    }, // a socket is closed
-    drain(ws) {
-      console.log("READY SOCKET");
-    }, // the socket is ready to receive more data
-  }, // handlers
 });
 
 console.log(`서버 오픈!`);
